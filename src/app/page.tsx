a989b62c56e51d7b70a916ef02271876940d067a
Wajeeha Arain <arainwajeeha715@gmail.com>
2025-05-19 11:23:42 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,15 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam repellendus autem repudiandae nesciunt ipsum voluptate enim quae ducimus sint, officia quidem aspernatur, nam, atque blanditiis quis modi magni ipsam distinctio?Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laudantium optio iste doloremque neque commodi, quia voluptates distinctio eius delectus odio praesentium voluptatibus omnis laborum, necessitatibus nulla sapiente labore. Id. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum obcaecati totam rerum, qui facilis vero odio adipisci culpa recusandae ad, illum consequuntur ipsum consectetur nulla voluptatem repellat distinctio harum. Aut. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum blanditiis necessitatibus quos placeat distinctio excepturi veniam nemo asperiores maxime expedita. Autem, nesciunt? Repellat, recusandae quas accusantium repudiandae sint illo vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia numquam eligendi illo assumenda. Rem nisi autem voluptatibus. Error temporibus, saepe voluptate, rem sint vitae consectetur quisquam alias quas dolores iure! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis adipisci minus veniam molestiae labore quos pariatur quis obcaecati, et nihil? Suscipit ullam aliquam eius consequuntur fugit adipisci sed placeat obcaecati. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nobis ratione repellendus consequuntur, ipsa soluta laboriosam quis quaerat officia fugit, nemo quos dicta excepturi deleniti ex voluptatibus ullam vel dolor? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque repellat, ea minima, iure accusantium nobis eos aspernatur accusamus vitae soluta placeat autem optio, atque ipsa deleniti in quisquam dignissimos? Accusantium!</p>
+
+
+
     </div>
-  );
+  )
 }
+
+export default page
